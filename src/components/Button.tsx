7bacb57fa728a05096c8a988694d5d1a6914701d
wichai.sw <48860871+wichaisw@users.noreply.github.com>
2025-01-07 00:03:32 +0700

    feat: add button with dynamic color

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+type IButtonProps = {
+  color: string;
+  text: string;
+  size?: number;
+};
+
+export default function Button({ color, text, size }: IButtonProps) {
+  const colorVariants: Record<string, string> = {
+    blue: "bg-blue-500 hover:bg-blue-700",
+    red: "bg-red-500 hover:bg-red-700",
+    orange: "bg-orange-500 hover:bg-orange-700",
+    gray: "bg-slate-500 hover:bg-slate-700",
+  };
+
+  return (
+    <button
+      className={`rounded px-4 py-2 font-bold text-white ${colorVariants[color]}`}
+    >
+      {text}
+    </button>
+  );
+}
