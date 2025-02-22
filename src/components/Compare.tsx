6d55f5ac767f76cbb422b75b3c051a4939a26438
wichai.sw <48860871+wichaisw@users.noreply.github.com>
2025-02-22 20:08:23 +0700

    refactor: remove unnecessary things & rename

---
+++ src/components/Compare.tsx
@@ -0,0 +1,7 @@
+import { Button } from "./Button";
+
+export function CompareButton() {
+  function compare() {}
+
+  return <Button text="Compare" color="blue" callback={() => {}} />;
+}
--- src/components/ModalButton.tsx
+++ src/components/ModalButton.tsx
@@ -2,12 +2,12 @@ import { useStore } from "@nanostores/react";
 import { isModalOpen } from "../states/modal";
 import { Button, type IButtonProps } from "./Button";
 
-export function ModalButton({ color, text, size }: IButtonProps) {
+export function ModalButton({ color, text }: IButtonProps) {
   const $isModalOpen: boolean = useStore(isModalOpen);
 
   return (
     <div onClick={() => isModalOpen.set(!$isModalOpen)}>
-      <Button color={color} text={text} size={size} />
+      <Button color={color} text={text} />
     </div>
   );
 }
