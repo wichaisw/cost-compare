3f1ad1b955b26cb45f658b3d4699dd88f11f54cf
wichai.sw <48860871+wichaisw@users.noreply.github.com>
2025-02-24 22:56:37 +0700

    feat: change summary text to table

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -4,6 +4,7 @@ import { sortedItemList } from "../states/items";
 import {
   getPriceDifference,
   getPriceDifferencePercent,
+  getPricePerUnit,
   getSavedCost,
 } from "../utils/priceCalculations";
 import { currency } from "../states/configs";
@@ -12,41 +13,61 @@ import type { ItemType } from "./Item";
 export function SummaryCard() {
   const $sortedItemList: ItemType[] = useStore(sortedItemList);
   const $currency: string = useStore(currency);
-  const [firstItem, secondItem] = $sortedItemList;
+  const [firstItem] = $sortedItemList;
 
   if ($sortedItemList.length < 2) {
     return null;
   }
 
   return (
-    <div className="w-full overflow-hidden rounded shadow-lg">
-      <div className="px-6 py-4">
-        <div className="mb-2 text-xl font-bold">Summary</div>
-        <div className="text-base text-gray-700">
-          <span>{firstItem.itemName} is the cheapest</span>
-          <br />
-          <span>{secondItem.itemName} is the second cheapest</span>
-          <br />
-          <div className="flew-row mt-2 flex justify-between">
-            <span>
-              {firstItem.itemName} is cheaper than {secondItem.itemName} by
-            </span>
-            <span>
-              {getPriceDifference(firstItem, secondItem)} {$currency}/Unit
-            </span>
-          </div>
-          <div className="flew-row flex justify-between">
-            <span>Which means</span>
-            <span>{getPriceDifferencePercent(firstItem, secondItem)} %</span>
-          </div>
-          <br />
-          <div className="flew-row mt-2 flex justify-between">
-            <span>Buying {firstItem.amount} units will save you</span>
-            <span>
-              {getSavedCost(firstItem, secondItem)} {$currency}
-            </span>
-          </div>
-        </div>
+    <div className="w-full">
+      <div className="mb-2 text-xl font-bold">
+        <span>{firstItem.itemName} is the cheapest choice!</span>
+        {/* <span>
+          Buying {firstItem.amount} units will save you{" "}
+          {getSavedCost(firstItem, secondItem)} {$currency}
+        </span> */}
+      </div>
+      <div className="flex w-full flex-col overflow-auto rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <table className="w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              <th className="border-blue-gray-100 bg-blue-gray-50 border-b p-4">
+                Item
+              </th>
+              <th className="border-blue-gray-100 bg-blue-gray-50 border-b p-4">
+                {$currency}/Unit
+              </th>
+              <th className="border-blue-gray-100 bg-blue-gray-50 border-b p-4">
+                The Cheapest Saved You by ...
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {$sortedItemList.map((item, index) => {
+              return (
+                <tr key={`${item.itemName}-${index}`}>
+                  <td className="border-blue-gray-50 border-b p-4">
+                    {item.itemName}
+                  </td>
+                  <td className="border-blue-gray-50 border-b p-4">
+                    {getPricePerUnit(item.price, item.amount)}
+                  </td>
+                  <td className="border-blue-gray-50 border-b p-4">
+                    {index === 0 ? (
+                      <p>Cheapest Choice!</p>
+                    ) : (
+                      <p>
+                        {getPriceDifference(firstItem, item)} {$currency}/Unit (
+                        {getPriceDifferencePercent(firstItem, item)} %)
+                      </p>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
