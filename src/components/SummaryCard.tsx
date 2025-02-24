65a6e0e7e7e59b8238cc4c7a15e0cc086cb0e912
wichai.sw <48860871+wichaisw@users.noreply.github.com>
2025-02-25 01:11:19 +0700

    feat: add select drowdown compare

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -9,11 +9,18 @@ import {
 } from "../utils/priceCalculations";
 import { currency } from "../states/configs";
 import type { ItemType } from "./Item";
+import { useEffect, useState } from "react";
 
 export function SummaryCard() {
   const $sortedItemList: ItemType[] = useStore(sortedItemList);
   const $currency: string = useStore(currency);
-  const [firstItem] = $sortedItemList;
+  const [cheapestItem, secondCheapestItem] = $sortedItemList;
+  const [compareTarget, setCompareTarget] =
+    useState<ItemType>(secondCheapestItem);
+
+  useEffect(() => {
+    setCompareTarget(secondCheapestItem);
+  }, [cheapestItem]);
 
   if ($sortedItemList.length < 2) {
     return null;
@@ -21,12 +28,35 @@ export function SummaryCard() {
 
   return (
     <div className="mb-32 w-full">
-      <div className="mb-2 text-xl font-bold text-white">
-        <span>{firstItem.itemName} is the cheapest choice!</span>
-        {/* <span>
-          Buying {firstItem.amount} units will save you{" "}
-          {getSavedCost(firstItem, secondItem)} {$currency}
-        </span> */}
+      <div className="mb-2 text-white">
+        <span className="text-xl font-bold">
+          {cheapestItem.itemName} is the Cheapest Choice!
+        </span>
+        <br />
+
+        <div>
+          Buying {cheapestItem.amount} units will save you{" "}
+          {getSavedCost(cheapestItem, compareTarget || secondCheapestItem)}{" "}
+          <p className="mx-1 inline">
+            <select
+              id="compare-target"
+              className="inline rounded-lg border border-gray-300 bg-gray-50 p-1 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              onChange={(event) =>
+                setCompareTarget($sortedItemList[Number(event.target.value)])
+              }
+              key={`itemList-select`}
+            >
+              {$sortedItemList.map((item, index) => {
+                if (index === 0) return null;
+                return (
+                  <option key={`${item.itemName}-${index}`} value={index}>
+                    {item.itemName}
+                  </option>
+                );
+              })}
+            </select>
+          </p>
+        </div>
       </div>
       <div className="flex w-full flex-col overflow-auto rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
         <table className="w-full min-w-max table-auto text-left">
@@ -62,8 +92,12 @@ export function SummaryCard() {
                       <p>It's Free!</p>
                     ) : (
                       <p>
-                        {getPriceDifference(firstItem, item)} {$currency}/Unit (
-                        {getPriceDifferencePercent(firstItem, item)} %)
+                        {getPriceDifference(cheapestItem, item)} {$currency}
+                        /Unit ({getPriceDifferencePercent(
+                          cheapestItem,
+                          item,
+                        )}{" "}
+                        %)
                       </p>
                     )}
                   </td>
