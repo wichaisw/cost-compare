81f66ac1f8513ae31c1232eb5a51cb73024a5bdf
wichai.sw <48860871+wichaisw@users.noreply.github.com>
2025-02-26 01:06:26 +0700

    deploy: use github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://wichaisw.github.io",
-  base: "/cost-compare",
-  output: "static",
 });
